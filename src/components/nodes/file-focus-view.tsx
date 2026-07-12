16bc7868d3195b84b03c818783a5c9820d606681
arun-j0 <arun2310kumar2002@gmail.com>
2026-07-12 16:32:04 +0530

    refactor: replace inline Drive SVG with DriveIcon in file-focus-view

--- src/components/nodes/file-focus-view.tsx
+++ src/components/nodes/file-focus-view.tsx
@@ -20,6 +20,7 @@ import { cn } from "@/lib/utils";
 import type { FileNodeData } from "@/lib/canvas-nodes";
 import { fileNodeService } from "@/services/file-node.service";
 import { useGooglePicker } from "@/hooks/use-google-picker";
+import { DriveIcon } from "@/components/ui/drive-icon";
 import { FileEmptyState } from "./file-empty-state";
 import { EditableField } from "./editable-field";
 import { FilePreview } from "./file-preview";
@@ -251,14 +252,7 @@ export function FileFocusView({
                       disabled={replacing || loading}
                       className="inline-flex h-11 items-center gap-2.5 rounded-lg border border-neutral-200 bg-white px-4 text-sm font-medium text-neutral-700 shadow-sm transition-all hover:bg-neutral-50 hover:shadow disabled:pointer-events-none disabled:opacity-50 active:scale-[0.99]"
                     >
-                      <svg width="16" height="16" viewBox="0 0 87.3 78" xmlns="http://www.w3.org/2000/svg">
-                        <path d="m6.6 66.85 3.85 6.65c.8 1.4 1.95 2.5 3.3 3.3l13.75-23.8h-27.5c0 1.55.4 3.1 1.2 4.5z" fill="#0066da"/>
-                        <path d="m43.65 25-13.75-23.8c-1.35.8-2.5 1.9-3.3 3.3l-25.4 44a9.06 9.06 0 0 0 -1.2 4.5h27.5z" fill="#00ac47"/>
-                        <path d="m73.55 76.8c1.35-.8 2.5-1.9 3.3-3.3l1.6-2.75 7.65-13.25c.8-1.4 1.2-2.95 1.2-4.5h-27.502l5.852 11.5z" fill="#ea4335"/>
-                        <path d="m43.65 25 13.75-23.8c-1.35-.8-2.9-1.2-4.5-1.2h-18.5c-1.6 0-3.15.45-4.5 1.2z" fill="#00832d"/>
-                        <path d="m59.8 53h-32.3l-13.75 23.8c1.35.8 2.9 1.2 4.5 1.2h50.8c1.6 0 3.15-.45 4.5-1.2z" fill="#2684fc"/>
-                        <path d="m73.4 26.5-12.7-22c-.8-1.4-1.95-2.5-3.3-3.3l-13.75 23.8 16.15 28h27.45c0-1.55-.4-3.1-1.2-4.5z" fill="#ffba00"/>
-                      </svg>
+                      <DriveIcon size={16} />
                       Replace from Drive
                     </button>
                     <Button variant="outline" size="lg" onClick={handleRemove}>
